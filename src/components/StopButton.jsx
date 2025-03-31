810889aef4be907f86b6afdd55f4dcfdde31655a
Eron Miranda <49223433+eronmiranda@users.noreply.github.com>
2025-03-31 02:13:46 -0600

    StopButton.jsx: Change svg to mui icon
    Use material icons instead of svg

--- src/components/StopButton.jsx
+++ src/components/StopButton.jsx
@@ -1,9 +1,9 @@
+import StopCircleRoundedIcon from '@mui/icons-material/StopCircleRounded';
+
 function StopButton(props) {
   return (
     <button {...props}>
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 7h6v6H7V7z" clip-rule="evenodd"/>
-      </svg>
+      <StopCircleRoundedIcon sx={{ fontSize: 95 }} />
     </button>
   )
 }
